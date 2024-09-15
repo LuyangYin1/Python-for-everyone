66f520c3c2bad2d9c858b91955134c8af1db2d79
POC USER <pocuser@satisfilabs.com>
2024-09-15 12:50:12 -0700

    Adding new code for class 2

---
+++ class-2/functions.py
@@ -0,0 +1,31 @@
+# Creating the function 
+def hello():
+    print("Hello World")
+
+hello()
+
+# Function with parameters 
+def adding_two_numbers(number1,number2):
+    print("Adding two numbers")
+    return number1 + number2 
+
+total = adding_two_numbers(2,3)
+print(total)
+
+# Base Case and Recursive Cases 
+# Factorial 5! = 5*4*3*2*1
+
+def factorial(n):
+    if n <= 0:
+        return 1 # base case 
+    return n * factorial(n-1) # recursive
+
+print(factorial(5))
+
+# def func1(x, y):
+#    return x+y
+
+def func2(a,b,c):
+    return adding_two_numbers(a*b,c)
+
+print(func2(1,2,3)) # prints 1 * 2 + 3  = 5  
