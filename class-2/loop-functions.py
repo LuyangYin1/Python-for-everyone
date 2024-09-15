b4633becd4bcbe65c07f0a8d40ac8a6da07c3a11
POC USER <pocuser@satisfilabs.com>
2024-09-15 12:59:52 -0700

    new codes

---
+++ class-2/loop-functions.py
@@ -0,0 +1,13 @@
+def sum_and_product(arr) :
+    sum = 0 
+    product = 1 
+    for num in arr:
+        sum += num 
+        product *= num 
+    return sum,product
+
+arr = [12, 23, 34, 2, 56, 90]
+print(sum_and_product(arr)) # sum and product 
+arr.add(10)
+print(sum_and_product(arr)) 
+print(type(sum_and_product(arr))) # tuple 
