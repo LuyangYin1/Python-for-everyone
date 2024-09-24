7b69fedc86956451149d83e65f2af8f3fd41b4dc
POC USER <pocuser@satisfilabs.com>
2024-09-23 19:38:38 -0700

    Update to the code

--- Class-3/ClassOverride.py
+++ Class-3/ClassOverride.py
@@ -4,32 +4,32 @@ class Animal:
         self.species = species
 
     def make_sound(self):
-        print("The animal makes a sound.")
+        return "The animal makes a sound."  #The animal makes a sound.
 
 class Dog(Animal):
     def __init__(self, name, breed):
         super().__init__(name, species="Dog")
 
     def make_sound(self):
-        print( "Woof!" )
+        return "Woof!" 
 
 class Cat(Animal):
     def __init__(self, name, color):
         super().__init__(name, species="Cat")
     def make_sound(self):
-        print( "Meow!" )
+        return "Meow!" 
 
 class Bird(Animal):
     def __init__(self, name, wingspan):
         super().__init__(name, species="Bird")
     def make_sound(self):
-        print( "Chirp!")
+        return "Chirp!"
 
 louis = Dog("Louis", "Labrador")
 print(louis.name)
 print(louis.species)
-print(louis.make_sound())
+print("Louis makes a sound:", louis.make_sound())
 bootsie = Cat("Bootsy", "Black")
-print(bootsie.make_sound())
+print("Bootsy makes a sound:", bootsie.make_sound())
 
     
