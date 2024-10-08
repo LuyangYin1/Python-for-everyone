74fcda65729ca40b96c31ab6e78b24e932f3f945
Avnit Bambah <abambah@gmail.com>
2024-10-08 07:30:40 -0700

    Adding code to rename and delete the files. Also to check try and except

---
+++ Class-5/file-operation.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+print("1. Reading the file without pandas")
+file = open("weather.txt", "r")
+with open(file) as file:
+    print(file.read())
+file.close()
+
+print("2. Read from the file into data frame")
+filename = 'weather.csv'
+df = pd.read_csv(filename)
+
+print("3. writing the file ")
+# to write the file we use 'w' 
+file_write = open('file_to_write','w')
+print(file_write.write(df))
+file_write.flush()
+file_write.close()
+
+# Deleting and Renaming the files 
+remove("new-file.txt")
+rename("file-to-write.txt","output.csv")
---
+++ Class-5/try-except.py
@@ -0,0 +1,12 @@
+try:
+    print(10/0)
+    text = "Hello"
+    number = int(text)
+except ValueError:
+    print("Code for Value Error")
+except ZeroDivisionError:
+    print("code for zero division")
+except:
+    print("code for other exceptions")
+finally:
+    print("Always Executed!")
