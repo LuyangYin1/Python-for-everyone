3b4fbe3e1e53094b5669dd0c02932d0d696e8f7f
POC USER <pocuser@satisfilabs.com>
2024-10-16 07:45:31 -0700

    updating plots

---
+++ Class-6/plot-style.py
@@ -0,0 +1,21 @@
+import datetime as dt 
+import yfinance as yf
+#import matplotlib as plt
+import matplotlib.pyplot as plt
+
+
+#yf.pdr_override()
+start = dt.datetime(2024,1,1)
+end = dt.datetime (2024,10,1)
+
+df = yf.download('TSLA',start,end)
+print(df['Adj Close'])
+df['Adj Close'].plot(label="TSLA")
+df_AAPL = yf.download('AAPL',start,end)
+print(df_AAPL['Adj Close'])
+df_AAPL['Adj Close'].plot(label="AAPL")
+plt.title("stock price")
+plt.ylabel("price")
+plt.legend(loc='upper left')
+plt.show()
+
