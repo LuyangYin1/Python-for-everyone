99c38cd778a837ec6c20ade7e5749673f97a9fe2
Avnit Bambah <pocuser@satisfilabs.com>
2025-02-05 19:44:33 -0800

    COde update

--- Class-1/Assignment-operator.py
+++ Class-1/Assignment-operator.py
@@ -1,12 +1,12 @@
 a = 10 
-# b = 3 
-# c = a % b 
-# print(c)
+b = 3 
+c = a % b 
+print(c)
 
 print ("before value of a is ", str(a))  # 10 
-# a += 1  # 10 + 1  = a (11)
-a = a + 1 
-print ("after value of a is ", str(a))  # 11 
+a += 2  # 10 + 1  = a (11)
+# a = a + 1 
+print ("after value of a is ", str(a))  # 20
 # print(a)
 
 # a -= 10 
--- Class-1/Sequences.py
+++ Class-1/Sequences.py
@@ -7,66 +7,55 @@ print(numbers) # [10, 20, 30, 40]
 print(names[1]) # Alex
 print(numbers[2]) # 30
 
-<<<<<<< HEAD
-
-
-
-
-# # print(numbers[2])
-# print(names[1:])
-# print(Mixed[:2])
-# print(Mixed[0:2])
-=======
 # print(numbers[2])
-print(names[1:])
-print(Mixed[:2])
-print(Mixed[0:2])
->>>>>>> f6061bc2ba31dd87bc74d1c6d31522b1a20945ac
+print(names[1:]) # ['Alex', 'Bob']
+print(Mixed[:2]) # ['John', 10]
+print(Mixed[0:2]) # ['John', 10]
 
 # # # Modifying elements 
-# numbers[2] = 13
-# print(numbers)
+numbers[2] = 13
+print(numbers)
 
 # # # Addition 
-# # numbers2 = [1,2,3]
-# # number3 = numbers + numbers2
-# # print(number3)
+numbers2 = [1,2,3]
+number3 = numbers + numbers2
+print(number3)
 
 # # # multiplication 
-# # number4 = numbers * 2
-# # print(number4)
-# # print(len(number4)) # 8 
-# # # Max min list(element)
-# # number4.append(1)
-# # print(number4.count(10))
-# # number4.sort()
-# # print(number4)
-# # number4.reverse()
-# # print(number4)
-# # print(max(number4))
-# # print(min(number4)) 
+number4 = numbers * 2
+print(number4)
+print(len(number4)) # 8 
+# Max min list(element)
+number4.append(1)
+print(number4.count(10))
+number4.sort()
+print(number4)
+number4.reverse()
+print(number4)
+print(max(number4))
+print(min(number4)) 
 
-# # # Tuples 
-#tpl = (10,20,30,40)
-# # print(tpl)
-# # # tuples are immutable and cannot be changed where as list can be chagned/ 
-# # # if we are asked to change tuple then we cannot do it.
-# # # difference in syntax is [] vs ()
+# # Tuples 
+tpl = (10,20,30,40)
+print(tpl)
+# # tuples are immutable and cannot be changed where as list can be chagned/ 
+# # if we are asked to change tuple then we cannot do it.
+# # difference in syntax is [] vs ()
 
-# # # Truple functions max len min 
+# # Truple functions max len min 
 
-# # Dictionaries 
-# # key-value pair 
-# dct = { "Name" : "John", 
-#        "Age" : "21",
-#        "Gender" : "Male "}
+# Dictionaries 
+# key-value pair 
+dct = { "Name" : "John", 
+       "Age" : "21",
+       "Gender" : "Male "}
 
-# dct2 = {"Name" : "John2", 
-#        "Age" : "31",
-#        "Gender" : "Male " }
+dct2 = {"Name" : "John2", 
+       "Age" : "31",
+       "Gender" : "Male " }
 
-# print(dct["Name"])
-# print(dct2["Name"])
+print(dct["Name"])
+print(dct2["Name"])
 
-# # # Functions 
-# # print(len(dct))
+# # Functions 
+# print(len(dct))
