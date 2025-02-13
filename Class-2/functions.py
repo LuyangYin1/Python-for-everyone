2badafcb9c057e6f99930080b10192ff4781cbdc
Avnit Bambah <pocuser@satisfilabs.com>
2025-02-12 18:32:17 -0800

    class changes

--- Class-2/functions.py
+++ Class-2/functions.py
@@ -14,7 +14,11 @@ print(total)
 
 #Default function
 def say(text="hello"):
-    print(text)
+    if text == "hello":
+        print("Please call the function with a text parameter")
+    else :
+        print("This is the correct way to call the function")
+        print(text)
 
 say()
 say("Hi")
@@ -22,19 +26,19 @@ say("Hi")
 
 
 # Base Case and Recursive Cases 
-# Factorial 5! = 5*4*3*2*1
+# # Factorial 5! = 5*4*3*2*1
 
-def factorial(n):
-    if n <= 0:
-        return 1 # base case 
-    return n * factorial(n-1) # recursive
+# def factorial(n):
+#     if n <= 0:
+#         return 1 # base case 
+#     return n * factorial(n-1) # recursive
 
-print(factorial(5))
+# print(factorial(5))
 
-# def func1(x, y):
-#    return x+y
+# # def func1(x, y):
+# #    return x+y
 
-def func2(a,b,c):
-    return adding_two_numbers(a*b,c)
+# def func2(a,b,c):
+#     return adding_two_numbers(a*b,c)
 
-print(func2(1,2,3)) # prints 1 * 2 + 3  = 5  
+# print(func2(1,2,3)) # prints 1 * 2 + 3  = 5  
