2763fe37dd3eb63e7869a0fa7bd1bb56499c328f
Avnit Bambah <abambah@google.com>
2025-02-24 18:32:58 +0000

    Additional changes to the websocket program

--- Class-3/socke.py
+++ Class-3/socke.py
@@ -1,8 +1,11 @@
+# Do not run this code. This is sample code for a student who wanted to do server networking in python. 
+#   Note : run the program, open chrome and go to webpage https://localhost:9998
+
 import socket
 import threading
 IP = '0.0.0.0'
 PORT = 9998
-def main()
+def main():
   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   server.bind((IP, PORT))
   server.listen(5)
@@ -13,6 +16,7 @@ def main()
     print(f'[*] Accepted connection from {address[0]}:{address[1]}')
     client_handler = threading.Thread(target=handle_client,args=(client,))
     client_handler.start()
+    client_handler.run()
 
 def handle_client(client_socket):
    with client_socket as sock:
@@ -22,3 +26,5 @@ def handle_client(client_socket):
 
 if __name__ == '__main__':
    main()
+   # after running the program open chrome and go to webpage https://localhost:9998
+
