0cbcb45548e2bb13337bdf6ffa025ecf312a91ff
GOD <sales@asbsolutionsgroup.com>
2025-03-12 15:09:05 -0700

    See a plot

---
+++ Class-5/plots.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+rng = np.random.default_rng(seed=42)
+X = rng.integers(0, 10, (4, 6))
+print(X)
+# plt.style.use('seaborn-whitegrid')
+plt.scatter(X[:, 0], X[:, 1], s=100)
