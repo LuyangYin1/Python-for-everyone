0670d7a0a1ed466f17030704e165608b3fcf9ae0
GOD <sales@asbsolutionsgroup.com>
2025-03-12 15:00:20 -0700

    numpy changes

--- Class-5/test.py
+++ Class-5/test.py
@@ -25,3 +25,37 @@ print(y)
 # Reshape an array
 print(y.reshape(6,5)) # works 
 # print(y.reshape(6,6)) # does not work
+
+# Flatten an array
+print(y.flatten())
+
+# Transpose an array
+print(y.T)
+
+z = np.random.randint(1,100,(5,6))
+# Concatenate arrays
+print(np.concatenate([y,z],axis=1))
+
+# Split an array
+a,b = np.split(x,[5],axis=1)
+print(a)
+print(b)
+
+# vertical split
+a,b = np.vsplit(x,[2])
+print(a)
+print(b)
+
+# horizontal split
+a,b = np.hsplit(x,[2])
+print(a)
+print(b)
+
+# Universal functions
+x = np.arange(4)
+print("x      =", x)
+print("x + 5  =", x + 5)
+print("x - 5  =", x - 5)
+print("x * 2  =", x * 2)
+print("x / 2  =", x / 2)
+print("x // 2 =", x // 2)  # floor division
