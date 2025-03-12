c2121f223ce0abb1e4f336b9b0a8d12fa83c70dc
GOD <sales@asbsolutionsgroup.com>
2025-03-12 11:08:19 -0700

    updating file operations: 100% (1/1), done Updating /data/inputs/iris.csv

--- Class-5/file-operation.py
+++ Class-5/file-operation.py
@@ -10,24 +10,24 @@ from os import remove, rename
 #file = wget.download(url2)
 
 
-# print("1. Reading the file without pandas")
+print("1. Reading the file without pandas")
 file = "weather.csv"
-# with open(file, "r",1,4096) as file:
-#     print(file.read())
+with open(file, "r",1,4096) as file:
+     print(file.read())
     
-# file.close()
+file.close()
 
-# print("2. Read from the file into data frame")
+print("2. Read from the file into data frame")
 filename = 'weather.csv'
 df = pd.read_csv(filename)
 print(df.head())
-# print("3. writing the file ")
+print("3. writing the file ")
 # # to write the file we use 'w' 
-# file_write = open('file_to_write','w')
-# print(file_write.write(str(df)))
-# file_write.flush()
-# file_write.close()
+file_write = open('file_to_write','w')
+print(file_write.write(str(df)))
+file_write.flush()
+file_write.close()
 
 # # Deleting and Renaming the files 
-# remove("new-file.txt")
-# rename("file-to-write.txt","output.csv")
+remove("new-file.txt")
+rename("file-to-write.txt","output.csv")
