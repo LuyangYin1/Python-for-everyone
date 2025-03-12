22b154f584b3e34074a04e0165afe14c3769d2bd
GOD <sales@asbsolutionsgroup.com>
2025-03-12 14:54:52 -0700

    update test

--- Class-5/test.py
+++ Class-5/test.py
@@ -1,3 +1,27 @@
+import numpy as np
+import pandas as pd
+
+print (np.random.random((10,10))) # float 
+print (np.random.randint(1,100,(10,10))) # int 
+print (np.eye(10)) # identity matrix
+print(np.empty((3))) # empty matrix
+# Compare this snippet from Class-5/exception.py:
 #print(10/0)
-text = "Hello"
-number = int(text)
+# text = "Hello"
+# number = int(text)
+
+x = np.random.randint(1,100,(10,10))
+print(x)
+# print(x[-1,1])
+# Array slicing 
+# x[start:stop:step,start:stop:step]
+print(x[0:5:1,0:5:2])
+print(x[0:5:1,0:5:2].shape)
+
+# Copy an array 
+y = x[0:5,0:6].copy()
+print(y)
+
+# Reshape an array
+print(y.reshape(6,5)) # works 
+# print(y.reshape(6,6)) # does not work
