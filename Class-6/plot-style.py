670460ba7e43922b117a2cd2f91d0fbad7eddc10
GOD <sales@asbsolutionsgroup.com>
2025-03-13 11:51:15 -0700

    tickwer updates

--- Class-6/plot-style.py
+++ Class-6/plot-style.py
@@ -1,3 +1,10 @@
+# pip install yfinance --upgrade --no-cache-dir 
+# pip install matplotlib
+# pip install numpy
+# pip install pandas
+# documentation webpage : https://yfinance-python.org/index.html
+# https://pypi.org/project/yfinance/
+
 import datetime as dt 
 import yfinance as yf
 #import matplotlib as plt
@@ -5,18 +12,19 @@ import matplotlib.pyplot as plt
 
 
 #yf.pdr_override()
-start = dt.datetime(2024,1,1)
-end = dt.datetime (2024,10,1)
+start = dt.datetime(2025,1,1)
+end = dt.datetime (2025,3,1)
 
 df = yf.download('TSLA',start,end)
-print(df['Adj Close'])
-df['Adj Close'].plot(label="TSLA")
+print(df['Close'])
+df['Close'].plot(label="TSLA")
 df_AAPL = yf.download('AAPL',start,end)
-print(df_AAPL['Adj Close'])
-df_AAPL['Adj Close'].plot(label="AAPL")
+print(df_AAPL['Close'])
+df_AAPL['Close'].plot(label="AAPL")
 plt.title("comparing stock price")
 plt.ylabel("price")
 plt.legend(loc='upper left')
+#plt.bar(df.index,df['Close'])
+#plt.bar(df_AAPL.index,df_AAPL['Close'])
 plt.show()
-
-plt.bar()
+#plt.bar()
