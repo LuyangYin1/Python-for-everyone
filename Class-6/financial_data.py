6c6d267b364cc4839fda08ebb1de472545c99a9a
GOD <sales@asbsolutionsgroup.com>
2025-03-13 11:58:36 -0700

    making the changes in the file

--- Class-6/financial_data.py
+++ Class-6/financial_data.py
@@ -1,23 +1,49 @@
+############################################
+# 1. Import the required libraries
+# 2. Define the start and end date for the data
+# 3. Download the data for the stock
+# 4. Print the data
+# 5. Save the data to an excel file
+# 6. Get the closing price for a specific date
+# 7. Plot the data
+# documentation webpage : https://yfinance-python.org/index.html
+# ##############################################
+# Follow the instructions in the comments and run the code in the terminal. 
+# pip install yfinance --upgrade --no-cache-dir
+# pip install matplotlib
+# pip install numpy
+# pip install pandas
+# pip install openpyxl
+
+
 import datetime as dt 
 import yfinance as yf
+import matplotlib.pyplot as plt
 
-#yf.pdr_override()
-start = dt.datetime(2024,1,1)
-end = dt.datetime (2024,9,1)
 
-# df = yf.download('AAPL',start,end)
-# print(df.head(5))
+start = dt.datetime(2025,1,1)
+end = dt.datetime (2025,4,1)
+
+df = yf.download('AAPL',start,end)
+print(df.head(5))
 
 initial_portfolio = ['AAPL','GOOG','SPY','MSFT','TSLA']
 df_portfolio = yf.download(initial_portfolio,start,end)
 print(df_portfolio.head(5))
 
 #pip install openpyxl
-#df.to_excel('apple2.xlsx')
-#df.to_excel('myportfolio.xlsx')
+df.to_excel('apple2.xlsx')
+df_portfolio.to_excel('myportfolio.xlsx')
 
 # To get closing pricing for APPL on a specific date 
 #print(df['Adj Close']['2024-01-08'])
 
-print((df_portfolio['Adj Close']['GOOG']['2024-02-08']))
-print((df_portfolio['Adj Close']['AAPL']['2024-02-08']))
+print((df_portfolio['Close']['GOOG']['2025-02-07']))
+print((df_portfolio['Close']['AAPL']['2025-02-07']))
+
+plt.plot(df['Close'])
+plt.plot(df_portfolio['Close']['AAPL']) 
+plt.plot(df_portfolio['Close']['GOOG'])
+plt.plot(df_portfolio['Close']['SPY'])
+plt.plot(df_portfolio['Close']['MSFT'])
+plt.plot(df_portfolio['Close']['TSLA'])
