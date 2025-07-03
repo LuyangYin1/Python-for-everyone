bf53390a80baac145d19be6a04e49fbedcfa8cec
Luyang Yin <luyangyin1@gmail.com>
2025-07-02 20:38:47 -0700

    hhhhh

--- Class-3/build-webpage.py
+++ Class-3/build-webpage.py
@@ -6,8 +6,8 @@ app = Flask(name_of_webpage)
 
 @app.route("/")
 def hello_world():
-    return "<html><head><body><h1>welcome to python for everyone class</h1></body></head></html>"
+    return "<html><head><body><h1>Hello World</h1></body></head></html>"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(debug=True)
